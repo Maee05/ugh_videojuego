dd559614333d1811a54d25fe339caee1bd073b62
Mae Alonso <maealonsoperez@gmail.com>
2025-02-03 12:46:30 +0100

    Unversioned files

---
+++ assets/tiles/Back Palm Tree Regular 01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Back Palm Tree Regular 01" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../images/Back Palm Tree Regular 01.png" width="64" height="64"/>
+</tileset>
---
+++ assets/tiles/Big Clouds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Big Clouds" tilewidth="32" tileheight="32" tilecount="42" columns="14">
+ <image source="../images/Big Clouds.png" width="448" height="101"/>
+</tileset>
